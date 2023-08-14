570e29fe9abdc416c7984bab03ef273573438f63
K1-1960 <k1.codingworld@gmail.com>
2023-08-13 21:37:52 -0700

    Un error arreglado.

--- src/middlewares/VerifyAPIKey.js
+++ src/middlewares/VerifyAPIKey.js
@@ -3,7 +3,7 @@ import keys from "../keys.js";
 export default function (req, res, next) {
   const apiKey = req.header("X-API-Key");
 
-  if (apiKey && MediaKeySession.includes(apiKey)) {
+  if (apiKey && keys.includes(apiKey)) {
     next();
   } else {
     res.status(401).json({ message: "Credencial de API inválida." });
